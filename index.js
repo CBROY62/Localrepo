cf76dc7f124a64bee7fc472cf06e7f4a2d34a338
Chandra Bhushan kumar <chandrabhushankumar27080@gmail.com>
2024-11-24 00:25:51 +0530

    add file

---
+++ index.js
@@ -0,0 +1,9 @@
+const data = [
+    { id: 1, name: 'John', age: 25 },
+    { id: 2, name: 'Jane', age: 30 },
+    { id: 3, name: 'Bob', age: 35 },
+    { id: 4, name: 'Alice', age: 20 },
+    { id: 5, name: 'Charlie', age: 40 },   
+];
+
+console.log(data ," this data is use full ");
